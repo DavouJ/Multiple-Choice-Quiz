2df3d5755b19e9d13dc4b3bdda5e1d5a3eed89ca
DavouJ <djjobbi@hotmailcom>
2024-01-02 01:17:47 +0000

    still working on logic, added questions and answers to the questions js file and touched up the html with bootstrap

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,66 @@
 const startBtn = document.querySelector("#start")
-const questions = document.querySelector("#questions")
+const questionsBox = document.querySelector("#questions")
 const startScrn = document.querySelector("#start-screen")
+const checkBtn = document.querySelector("#check")
+const resultBtn = document.querySelector("#results")
+const nextBtn = document.querySelector("#next")
+const endBox = document.querySelector("#end-screen")
 
-start.addEventListener("click", function() {
+let options = [
+option1 = document.querySelector("#option1"),
+option2 = document.querySelector("#option2"),
+option3 = document.querySelector("#option3"),
+option4 = document.querySelector("#option4")
+]
+
+let round = 0
+
+let correctBtn
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+}
+
+startBtn.addEventListener("click", function() {
+    
     startScrn.setAttribute("class", "container-fluid py-5 hide")
-    questions.setAttribute("class", "container-fluid py-5 start")
+    questionsBox.setAttribute("class", "container-fluid py-5 start")
+    mapQA() 
+    round++
+    console.log(round)
 });
+
+function mapQA(){
+    var order = []
+    questionTitle.textContent = `Question ${round + 1}: ` + getQuestion(round)  
+    options[getRandomInt(3)].setAttribute("value", questions[getRandomInt(round)].correctAnswer)
+    
+}
+
+function checkAns()
+{
+
+}
+
+checkBtn.addEventListener("click", function() {
+    checkBtn.setAttribute("class", "btn btn-primary btn-lg hide")
+    checkAns()
+    nextBtn.setAttribute("class", "btn col-3 btn-primary btn-lg start")
+});
+
+nextBtn.addEventListener("click", function() {
+    if(round < questions.length){  
+        mapQA() 
+        checkBtn.setAttribute("class", "btn btn-primary btn-lg hide")
+        resultBtn.setAttribute("class", "btn col-3 btn-primary btn-lg start")  
+        round++
+        console.log(round)
+    }
+});
+resultBtn.addEventListener("click", function(){
+    
+    endBox.setAttribute("class", "container-fluid py-5 start")
+    questionsBox.setAttribute("class", "container-fluid py-5 hide")
+})
+
+
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,34 @@
-const questions = document.querySelector("#questions")
+const questionTitle = document.querySelector("#question-title")
+
+const questions = [
+    {
+    question: "What does CSS stand for?",
+    correctAnswer: "Cascading Style Sheet",
+    incorrectAnswer: ["Cascading Syntax Sheet", "Conditional Syntax Script", "Continual System Script"]
+},
+{
+    question: "Which of the following are a HTML framework?",
+    correctAnswer: "Tailwind",
+    incorrectAnswer: ["Bootlace", "Javascript", "C#"]
+},
+{
+    question: "Which three are the most commonly used together for web-development?",
+    correctAnswer: "HTML, CSS & JavaScript",
+    incorrectAnswer: ["Java, C++ & HTML ", "Bootstrap, Java, and CSS", "HTML, CSS & Java"]
+},
+{
+    question: "What is an 'if' statement?",
+    correctAnswer: "A conditional statement",
+    incorrectAnswer: ["An iterative statement", "A switch case", "A method"]
+},
+{
+    question: "What Git command is used to define a change?",
+    correctAnswer: "git commit",
+    incorrectAnswer: ["git add", "git clone", "git push"]
+}]
+
+
+function getQuestion(num){
+    
+    return questions[num].question;
+}
